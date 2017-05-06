5c675755544bac9b411f3a92d8e092686759a688
Zhengqi Xi <zhengqixi@gmail.com>
2017-05-06 19:44:54 -0400

    Web service set up

--- engine/response.py
+++ engine/response.py
@@ -1,1 +1,7 @@
-#/usr/bin/python
+def application(environ, start_response):
+    status = '200 OK'
+    output = b'Goodbye World!\n'
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+    return [output]
