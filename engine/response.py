17b1bebc7551e618d2fe889c5f3102002f80ad73
Zhengqi Xi <zhengqixi@gmail.com>
2017-05-07 00:15:02 -0400

    sigh...

--- engine/response.py
+++ engine/response.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = 'wsgi.multithread = %s' % repr(environ['wsgi.multithread'])
+    output = b'hello\n'
 
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
